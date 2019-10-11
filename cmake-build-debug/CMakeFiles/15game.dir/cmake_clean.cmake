88b742e7337545e6575fda16090f5d299fa5e6cb
Konstantinos Panteli (PGT) <b9050880@newcastle.ac.uk>
2019-10-11 10:49:07 +0100

    Initialisation, first commit

---
+++ cmake-build-debug/CMakeFiles/15game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15game.exe"
+  "15game.exe.manifest"
+  "15game.lib"
+  "15game.pdb"
+  "CMakeFiles/15game.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
